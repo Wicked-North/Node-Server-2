477300ca61df50e6fad27e196fd2475c7fb6b394
Sagnik <sagnik.das1027@gmail.com>
2020-07-06 18:18:32 +0530

    session Passport

--- models/user.js
+++ models/user.js
@@ -1,23 +1,19 @@
 const mongoose=require('mongoose');
 var Schema=mongoose.Schema;
 
+var passportLocalMongoose=require('passport-local-mongoose');
+
 var User=new Schema({
 
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
+    
     admin:{
         type:Boolean,
         defaule:false
     }
 
 });
+
+User.plugin(passportLocalMongoose);
 
 
 module.exports=mongoose.model('users',User);
